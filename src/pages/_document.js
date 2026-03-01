4169b4bdeafdf0dde40e1593b728f44f7d36e173
dadson6543-hub <dadson6543@gmail.com>
2026-03-01 15:27:39 +0500

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,1 +1,19 @@
+// pages/_document.js
+import { Html, Head, Main, NextScript } from 'next/document';
 
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        {/* Google Search Console Verification */}
+        <meta name="google-site-verification" content="9Y1k_hxZb14Hv-adepMUyevlFUtZ8gEgQNlMzGUCwO0" />
+        
+        {/* Baaki SEO meta tags optional */}
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
